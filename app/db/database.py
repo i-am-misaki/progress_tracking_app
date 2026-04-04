a38d382a89692d136674f0d7d38eb465ebd43178
i-am-misaki <mkmtnm@yahoo.com>
2026-04-04 21:23:39 +0900

    Revert "ADD: env.sample 作成、env 呼び出し"
    This reverts commit d76fa2c5df770143aeb769a0fc237a76d691faeb.

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.core.config import DATABASE_URL
-
-engine =create_engine(DATABASE_URL)
+engine =create_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
