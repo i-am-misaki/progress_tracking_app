37fd5de46f71edb55ab71732ea1abf71998a724c
i-am-misaki <mkmtnm@yahoo.com>
2026-04-11 23:11:42 +0900

    ADD: アカウント有効無効判定カラム追加

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, UUID
+from sqlalchemy import Column, Integer, String, DateTime, func, UUID, Boolean
 from uuid import uuid4
 
 from app.db.database import Base
@@ -12,6 +12,7 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String(245), unique=True, nullable=False)
     password = Column(String, nullable=False)
+    disabled = Column(Boolean, default=False, nullable=False, server_default="false")
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
