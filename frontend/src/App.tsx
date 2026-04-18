b6d83aa09072410ee1f3efbff92aceefefbdf0c0
i-am-misaki <mkmtnm@yahoo.com>
2026-04-18 11:21:45 +0900

    ADD: ルート追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/login.tsx';
 import ProjectProgressList from './pages/project_progress_list.tsx';
+import PasswordForget from './pages/password_forget.tsx';
+import PasswordResetMailed from './pages/password_reset_mailed.tsx';
 import PasswordReset from './pages/password_reset.tsx';
 
 
@@ -12,8 +14,12 @@ function App() {
         {/* ログイン画面 */}
         <Route path="/login" element={<Login />} />
 
+        {/* パスワードリセットメールアドレス入力画面 */}
+        <Route path="/password_forget" element={<PasswordForget />} />
+        {/* パスワードリセットメール送信完了画面 */}
+        <Route path="/password_reset_mailed" element={<PasswordResetMailed />} />
         {/* パスワードリセット画面 */}
-        <Route path="/password-reset" element={<PasswordReset />} />
+        <Route path="/password_reset" element={<PasswordReset />} />
 
         {/* 案件進捗一覧画面 */}
         <Route path="/projects" element={<ProjectProgressList />} />
