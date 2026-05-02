04878772506cdbc07285671bb852e5fcb165fd89
i-am-misaki <mkmtnm@yahoo.com>
2026-05-02 17:22:27 +0900

    FEATURE: フロントエンドデータ受け取り表示実装

--- frontend/src/pages/project_progress_list.tsx
+++ frontend/src/pages/project_progress_list.tsx
@@ -1,14 +1,35 @@
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
 import { AddIcon } from '../assets/icons/AddIcon';
 import { EditIcon } from '../assets/icons/EditIcon';
 import { DeleteIcon } from '../assets/icons/DeleteIcon';
+import type { Project } from '../types/project';
 
 
 
 
 export default function ProjectProgressList() {
     const navigate = useNavigate();
+    const [projects, setProjects] = useState<Project[]>([]);
+    // 読み込み状態管理
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('/api/member/projects')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`サーバーエラー: ${res.status}`)
+                }
+                return res.json();
+            })
+            .then(data => setProjects(data)
+            )
+            .catch(err => console.error('案件一覧の取得に失敗: ', err))
+            .finally(() => setIsLoading(false))
+    }, []);
+
 
     return (
         <div className="h-screen w-screen">
@@ -33,66 +54,79 @@ export default function ProjectProgressList() {
                                 <span className="text-base" style={{ fontFamily: "'Changa', sans-serif" }}>Project</span>
                             </button>
                         </div>
-                        <div className="max-h-[550px] overflow-y-auto overflow-x-auto w-full px-4">
-                            <table className="w-full table-auto border-collapse">
-                                <thead className="sticky top-0 z-10 bg-gray-400">
-                                    <tr className="text-xl text-left" style={{ fontFamily: "'Changa', sans-serif" }}>
-                                        <th className="w-64 p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            <p>Project Name</p>
-                                        </th>
-                                        <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            <p>Client</p>
-                                        </th>
-                                        <th className="w-32 p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            <p>ETA</p>
-                                        </th>
-                                        <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            <p>Status</p>
-                                        </th>
-                                        <th className="p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            <p>Latest Progress</p>
-                                        </th>
-                                        <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
-                                            {/* 編集/削除 ボタン表示列 */}
-                                            <p></p>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-gray-400/50">
-                                    <tr className="border-b border-slate-200">
-                                        <td className="w-64 p-2">
-                                            <p>案件名</p>
-                                        </td>
-                                        <td className="w-40 p-2">
-                                            <p>依頼元</p>
-                                        </td>
-                                        <td className="w-32 p-2">
-                                            <p>2026/7/7</p>
-                                        </td>
-                                        <td className="w-40 p-2">
-                                            <p>IN PROGRESS</p>
-                                        </td>
-                                        <td className="p-2">
-                                            <p>コーディング中</p>
-                                        </td>
-                                        <td className="w-40 p-2">
-                                            <div className="flex justify-center items-center gap-3" style={{ fontFamily: "'Changa', sans-serif" }}>
-                                                <button type="button"
-                                                        className=" text-white py-1 px-2 cursor-pointer transition-colors hover:text-amber-400"
-                                                    >
-                                                    <EditIcon />
-                                                </button>
-                                                <button type="button"
-                                                        className=" text-white py-1 px-2 cursor-pointer transition-colors hover:text-amber-400"
-                                                    >
-                                                    <DeleteIcon />
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        { isLoading ? (
+                                <p className="text-white text-center">Loading...</p>
+                            ) : projects && projects.length > 0 ? (
+                                <div className="max-h-[550px] overflow-y-auto overflow-x-auto w-full px-4">
+                                    <table className="w-full table-auto border-collapse">
+                                        <thead className="sticky top-0 z-10 bg-gray-400">
+                                            <tr className="text-xl text-left" style={{ fontFamily: "'Changa', sans-serif" }}>
+                                                <th className="w-64 p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    <p>Project Name</p>
+                                                </th>
+                                                <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    <p>Client</p>
+                                                </th>
+                                                <th className="w-32 p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    <p>ETA</p>
+                                                </th>
+                                                <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    <p>Status</p>
+                                                </th>
+                                                <th className="p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    <p>Latest Progress</p>
+                                                </th>
+                                                <th className="w-40 p-2 shadow-[inset_0_-2px_0_0_#000000]">
+                                                    {/* 編集/削除 ボタン表示列 */}
+                                                    <p></p>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-gray-400/50">
+                                            { projects.map((project) =>
+                                                <tr className="border-b border-slate-200">
+                                                    <td className="w-64 p-2">
+                                                        <p>{project.project_name}</p>
+                                                    </td>
+                                                    <td className="w-40 p-2">
+                                                        <p>{project.client}</p>
+                                                    </td>
+                                                    <td className="w-32 p-2">
+                                                        <p>{project.eta}</p>
+                                                    </td>
+                                                    <td className="w-40 p-2">
+                                                        <p>{project.status}</p>
+                                                    </td>
+                                                    <td className="p-2">
+                                                        <p>{project.latest_progress}</p>
+                                                    </td>
+                                                    <td className="w-40 p-2">
+                                                        <div className="flex justify-center items-center gap-3" style={{ fontFamily: "'Changa', sans-serif" }}>
+                                                            <button type="button"
+                                                                    className=" text-white py-1 px-2 cursor-pointer transition-colors hover:text-amber-400"
+                                                                >
+                                                                <EditIcon />
+                                                            </button>
+                                                            <button type="button"
+                                                                    className=" text-white py-1 px-2 cursor-pointer transition-colors hover:text-amber-400"
+                                                                >
+                                                                <DeleteIcon />
+                                                            </button>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )};
+                                        </tbody>
+                                    </table>
+                                </div>
+                            ) : (
+                                <div className="flex flex-col justify-center items-center py-20 bg-gray-400/50 rounded-md mx-4">
+                                    <p className="text-xl text-black/90" style={{ fontFamily: "'Changa', sans-serif" }}>
+                                        No data available.
+                                    </p>
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
--- frontend/src/types/project.tsx
+++ frontend/src/types/project.tsx
@@ -1,3 +1,7 @@
+type UUID = string;
+
+
+
 /**
  * 案件新規登録情報インターフェース
  *
@@ -11,8 +15,6 @@
  * @property {string} priority        - 優先度
  * @property {string} progress        - 進捗状況
  */
-
-type UUID = string;
 export interface ProjectRegister {
     project_name: string;
     project_summary: string;
@@ -24,14 +26,25 @@ export interface ProjectRegister {
     progress: string;
 }
 
-
+/**
+ * 案件一覧画面表示インターフェース
+ *
+ * @interface Project
+ * @property {string} project_uuid    - 案件のUUID
+ * @property {string} project_name    - 案件名
+ * @property {string} client          - クライアント名
+ * @property {string} eta             - 納期
+ * @property {string} pic             - 担当者
+ * @property {string} status          - 進捗状態
+ * @property {string} latest_progress - 最新の進捗
+ */
 export interface Project {
     project_uuid: string;
     project_name: string;
     client: string;
-    eta: string | null;
-    pic: UUID | null;
+    eta: string;
+    pic: UUID;
     status: string;
     priority: string;
-    progress: string;
+    latest_progress: string;
 }
