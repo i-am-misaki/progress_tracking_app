d2ff6fa258935bb67e0ff040e3e25aea3a210047
i-am-misaki <mkmtnm@yahoo.com>
2026-05-02 19:26:53 +0900

    FIX: 案件登録クエリ修正

--- app/crud/project.py
+++ app/crud/project.py
@@ -1,7 +1,13 @@
-from app.db.database import SessionLocal
+from typing import Optional
+from datetime import date
 
+from app.db.database import SessionLocal
 from app.schemas.project import ProjectRegistration
 from app.models.project import Project
+from app.models.user import User
+from app.models.project_assignment import ProjectAssignments
+from app.models.process_tracking import ProcessTracking
+from app.crud.auth import get_user_by_uuid
 
 
 async def add_project(request: ProjectRegistration) -> None:
@@ -12,17 +18,48 @@ async def add_project(request: ProjectRegistration) -> None:
         request(ProjectRegistration) : 案件登録登録スチーマクラス
     """
     db = SessionLocal()
+
+    # ETA の型を datetime.date 型に変換
+    eta: Optional[date] = None
+    if request.eta:
+        eta = request.eta.strptime("%Y-%m-%d")
+
+    # 案件登録
     new_project = Project(
         title=request.project_name,
         summary=request.project_summary,
         priority=request.priority,
         status=request.status,
-        delivery_date=request.eta,
-        client=request.client,
-        pic=request.pic,
-        progress=request.progress
+        delivery_date=eta,
+        client=request.client
     )
     db.add(new_project)
-    db.commit()
-    db.refresh(new_project)
+    db.flush()
+
+    # 案件担当者情報の登録
+    project_pic = db.query(User).filter(User.uuid == request.pic).first()
+    if project_pic:
+        new_assignment = ProjectAssignments(
+            project_id=new_project.id,
+            user_id=project_pic.id
+        )
+        db.add(new_assignment)
+
+    # 進捗登録
+    if request.progress:
+        new_process = ProcessTracking(
+            content=request.progress,
+            project_id=new_process.id,
+            # ログインユーザーのIDを格納
+            user_id=1
+        )
+        db.add(new_process)
+
+    try:
+        db.commit()
+        db.refresh(new_project)
+        return new_project
+    except Exception as e:
+        db.rollback()
+        raise e
 
