719c0fe3301ce56697919d41652a3b730482dd89
i-am-misaki <mkmtnm@yahoo.com>
2026-05-02 18:12:57 +0900

    FEATURE: 条件に一致する全ての案件情報取得処理実装

--- app/crud/project.py
+++ app/crud/project.py
@@ -26,3 +26,39 @@ async def add_project(request: ProjectRegistration) -> None:
     db.commit()
     db.refresh(new_project)
 
+
+async def get_projects() -> list[Project]:
+    """
+    projects テーブルから下記の条件に一致するレコードデータを全て取得する
+    ・ deleted_at が null
+    ・ status が COMPLETED ではないもの
+
+    Returns
+        list[Project] : 抽出したレコードリストを返す
+    """
+    db = SessionLocal()
+    query_results = (
+        db.query(Project)
+            .filter(Project.deleted_at == None, Project.status != "COMPLETED")
+            .all()
+    )
+
+    project_list: list = []
+    for p in query_results:
+        latest_track = None
+        if p.process_trackings:
+            latest_track = sorted(p.process_trackings, key=lambda x: x.id, reverse=True)[0]
+
+        pic_user = p.project_assign[0].user.name if p.project_assign else ""
+
+        p_model = Project(
+            project_uuid=p.project.uuid,
+            project_name=p.project_name,
+            client=p.client,
+            eta=p.delivery_date.strftime("%Y%m%d") if p.delivery_date else "",
+            pic=pic_user.uuid,
+            status=p.status,
+            latest_progress=latest_track.progress_content if latest_track else ""
+        )
+        project_list.append(p_model)
+    return project_list
